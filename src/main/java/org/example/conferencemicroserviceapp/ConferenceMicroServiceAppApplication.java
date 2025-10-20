8705bae5bc29abea99100ccd0701d499f4beb1ad
ElBadry <mohammedbadry2003@gmail.com>
2025-10-20 11:02:23 +0100

    First commit

---
+++ src/main/java/org/example/conferencemicroserviceapp/ConferenceMicroServiceAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.conferencemicroserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceMicroServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceMicroServiceAppApplication.class, args);
+    }
+
+}
